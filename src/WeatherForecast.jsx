0002db90688801bb1dd2a481fd2a81d473acfdef
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-03-25 18:22:40 -0700

    weather forecasts

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+
+<div className="weather">
+  <h2>Day of the Week</h2>
+  <img src="" alt="" />
+  <p><span>conditions: </span>current weather conditions</p>
+  <p><span>time: </span>time of day</p>
+</div>
